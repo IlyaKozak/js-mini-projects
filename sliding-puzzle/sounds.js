12e3e43b692073d5faf4859d37cd254b4112cd8e
Ilya Kozak <kozak@tut.by>
2020-11-07 11:22:09 +0300

    fix: update sliding-puzzle with bundle build

--- sliding-puzzle/sounds.js
+++
@@ -1,7 +0,0 @@
-const moveTileSound = new Audio('./sounds/tile-move.mp3');
-const nomoveTileSound = new Audio('./sounds/no-move.mp3');
-const shuffleSound = new Audio('./sounds/shuffle.mp3');
-const gameMusic = new Audio('./sounds/cheeky_monkey.mp3');
-gameMusic.loop = true;
-const winSound = new Audio('./sounds/win.mp3');
-const clickSound = new Audio('./sounds/mouse_click.mp3');
